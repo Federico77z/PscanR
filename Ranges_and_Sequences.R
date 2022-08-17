5023c8c8c4abd69f2239db9511da26a80641eef9
Federico Zambelli <federico.zambelli@unimi.it>
2022-08-17 13:06:13 +0200

    Experiments on annotation and sequences

--- Ranges_and_Sequences.R
+++ Ranges_and_Sequences.R
@@ -1,12 +1,17 @@
 library(GenomicFeatures)
 library(GenomicRanges)
+library(BSgenome)
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(BSgenome.Mmusculus.UCSC.mm10)
 
-txdb <- makeTxDbFromUCSC(genome="sacCer2", tablename="ensGene")
 
-txdb <- makeTxDbFromUCSC(genome="hg19", tablename="knownGene",
-                 transcript_ids=NULL,
-                 circ_seqs=NULL,
-                 url="http://genome.ucsc.edu/cgi-bin/",
-                 goldenPath.url="https://hgdownload.soe.ucsc.edu/",
-                 taxonomyId=NA,
-                 miRBaseBuild=NA)
+txdb <- makeTxDbFromUCSC(genome="hg38", tablename="ncbiRefSeqCurated") #import gtf annotation from ucsc
+seqlevels(txdb) <- seqlevels(txdb)[1:24] #use only annotation on canonical chromosomes
+
+export(txdb, "./Annotation/hg38.ucsc.bed", format = "BED")  #export
+export(txdb, "./Annotation/hg38.ucsc.bed", format = "GTF")
+
+prom_seq <- getPromoterSeq(asBED(txdb), BSgenome.Hsapiens.UCSC.hg38, upstream = 500, downstream = 0) #get promoter sequences
+#prom_seq <- extractUpstreamSeqs(BSgenome.Hsapiens.UCSC.hg38, txdb, width = 500)
+names(prom_seq) <- keys(txdb, "TXNAME")
+writeXStringSet(prom_seq, "./FASTA/hg38_prom_500_0.fasta")
