3cfa31051212e4ad702d01677bdd9b00c9eb9d06
Federico Zambelli <federico.zambelli@unimi.it>
2022-08-17 16:14:03 +0200

    Further experiments. Now promoters work!

--- Ranges_and_Sequences.R
+++ Ranges_and_Sequences.R
@@ -11,7 +11,28 @@ seqlevels(txdb) <- seqlevels(txdb)[1:24] #use only annotation on canonical chrom
 export(txdb, "./Annotation/hg38.ucsc.bed", format = "BED")  #export
 export(txdb, "./Annotation/hg38.ucsc.bed", format = "GTF")
 
-prom_seq <- getPromoterSeq(asBED(txdb), BSgenome.Hsapiens.UCSC.hg38, upstream = 500, downstream = 0) #get promoter sequences
+#trnames <- transcripts(txdb, columns = "TXNAME")$TXNAME
+#trasbed <- asBED(txdb)
+
+#txstrand <- select(txdb, trnames, columns = c("TXNAME", "TXCHROM", "TXSTART", "TXEND", "TXSTRAND", "TXTYPE"), keytype = "TXNAME")$TXSTRAND
+#txstrand_minus <- which(txstrand == "-")
+#txstrand_plus <- which(txstrand == "+")
+
+#select(txdb, trnames[txstrand_minus], columns = c("TXNAME", "TXCHROM", "TXSTART", "TXEND", "TXSTRAND", "TXTYPE"), keytype = "TXNAME", )
+
+prom_rng <- promoters(txdb, upstream = 500, downstream = 0, use.names = TRUE) #Define promoters
+
+#prom_seq <- getPromoterSeq(asBED(txdb), BSgenome.Hsapiens.UCSC.hg38, upstream = 500, downstream = 0) #get promoter sequences
 #prom_seq <- extractUpstreamSeqs(BSgenome.Hsapiens.UCSC.hg38, txdb, width = 500)
-names(prom_seq) <- keys(txdb, "TXNAME")
-writeXStringSet(prom_seq, "./FASTA/hg38_prom_500_0.fasta")
+#names(prom_seq) <- keys(txdb, "TXNAME")
+
+prom_seq <- getSeq(x = BSgenome.Hsapiens.UCSC.hg38, prom_rng)  #Get promoter sequences
+prom_seq_unique <- unique(prom_seq)
+
+#tx_plus_pos <- which(as.character(strand(prom_rng)) == "+")
+#tx_minus_pos <- which(as.character(strand(prom_rng)) == "-")
+
+writeXStringSet(prom_seq, "./FASTA/hg38_prom_500_0.fasta") #Write multifasta file
+writeXStringSet(prom_seq_unique, "./FASTA/hg38_prom_500_0.unique.fasta")
+
+
