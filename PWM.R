5b7af4459e09e3ea4eea2f3024ffc548a5fc3de6
Federico Zambelli <federico.zambelli@unimi.it>
2022-08-18 15:01:13 +0200

    Further experiments with PWMs

--- PWM.R
+++ PWM.R
@@ -11,3 +11,10 @@ opts[["tax_group"]] <- "vertebrates"
 
 J2020 <- getMatrixSet(JASPAR2020, opts)
 J2016 <- getMatrixSet(JASPAR2016, opts)
+
+J2016_pwm <- toPWM(J2016, type = "prob", pseudocounts = 0.1)
+J2020_pwm <- toPWM(J2020, type = "prob", pseudocounts = 0.1)
+
+pwm <- Matrix(J2020_pwm$MA0818.1)
+
+hits <- matchPWM(pwm, prom_seq_unique$NR_046018.2, min.score = 0, with.score = TRUE)
