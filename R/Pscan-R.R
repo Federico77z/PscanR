dc0e852606e69cb46170bbfadb966f63781100a3
dianabetelli <diana.betelli@studenti.unimi.it>
2024-11-25 16:47:39 +0100

    Package documentation

---
+++ R/Pscan-R.R
@@ -0,0 +1,34 @@
+#' Pscan-R: An R Interface for the `PScan` Tool
+#' 
+#' @description
+#' `Pscan-R` provides an R implementation of the `Pscan` web platform for 
+#' transcription factor binding site motif analysis. 
+#' It includes function to build the background for statistical analyses, 
+#' to scan the promoter sequences, and data visualization.  
+#' The package is designed to work with Bioconductor data objects.
+#' 
+#' @section Main functions:
+#' \itemize{
+#'    \item \code{\link{ps_build_bg_from_file}}: Build background matrices 
+#'    from a file. 
+#'    \item \code{\link{pscan}}: Pscan algorithm utilization on a set 
+#'    of gen promoters.
+#'    \item \code{\link{ps_result_table}}: Data visualization. 
+#' }
+#' 
+#' @docType package
+#' @name Pscan-R
+#' 
+#' @references 
+#' `Pscan` platform: \url{http://159.149.160.88/pscan/}
+#' 
+#' Zambelli F, Pesole G, Pavesi G. Pscan: finding over-represented transcription 
+#' factor binding site motifs in sequences from co-regulated or co-expressed 
+#' genes. Nucleic Acids Res. 2009 Jul;37(Web Server issue):W247-52. 
+#' doi: 10.1093/nar/gkp464. Epub 2009 May 31. PMID: 19487240; PMCID: PMC2703934.
+#' 
+#' @author 
+#' Federico Zambelli [aut], Giulio Pavesi [cre]
+#' 
+#' Maintainer: Federico Zambelli <federico.zambelli@unimi.it>
+NULL
