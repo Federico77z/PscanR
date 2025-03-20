26c081632265d1373623b00ce4c8d7d790f50566
dianabetelli <diana.betelli@studenti.unimi.it>
2025-03-20 10:09:16 +0100

    New Backgrounds

--- inst/script/promoters.R
+++
@@ -1,3 +0,0 @@
-txdb <- txdbmaker::makeTxDbFromUCSC(genome="hg38", tablename="ncbiRefSeqCurated")
-seqlevels(txdb) <- seqlevels(txdb)[1:24]
-prom_rng <- GenomicFeatures::promoters(txdb, upstream = 200, downstream = 50, use.names = TRUE)
