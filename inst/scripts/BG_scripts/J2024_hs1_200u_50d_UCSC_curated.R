f77e55381221bb6b478679527d27ac54d147cf22
dianabetelli <diana.betelli@studenti.unimi.it>
2025-04-13 21:50:26 +0200

    hs1 BG scripts modification

--- inst/scripts/BG_scripts/J2024_hs1_200u_50d_UCSC_curated.R
+++ inst/scripts/BG_scripts/J2024_hs1_200u_50d_UCSC_curated.R
@@ -1,5 +1,4 @@
-hs1_path <- system.file('extdata/BG_scripts', 'GCF_009914755.1_T2T-CHM13v2.0_genomic.gff.gz', package = 'PscanR')
-gff <- rtracklayer::import(hs1_path)
+#gff <- rtracklayer::import('raw_data/GCF_009914755.1_T2T-CHM13v2.0_genomic.gff.gz')
 chrominfo <- GenomeInfoDb::getChromInfoFromNCBI("T2T-CHM13v2.0")
 GenomeInfoDb::seqlevels(gff) <- setNames(chrominfo$SequenceName, chrominfo$RefSeqAccn)
 GenomeInfoDb::seqinfo(gff) <- GenomeInfoDb::Seqinfo(genome="T2T-CHM13v2.0")
@@ -10,6 +9,10 @@ GenomeInfoDb::seqlevels(txdb) <- GenomeInfoDb::seqlevels(txdb)[1:24] #use only a
 prom_rng <- GenomicFeatures::promoters(txdb, upstream = 200, downstream = 50, use.names = TRUE) 
 prom_seq <- Biostrings::getSeq(x = BSgenome.Hsapiens.UCSC.hs1::BSgenome.Hsapiens.UCSC.hs1, 
                                prom_rng) #promoter sequences
+
+# Remove sequences which identifier starts with XR or XM. Remove also sequences without name
+
+prom_seq <- prom_seq[!grepl('^X[MR]_', names(prom_seq)) & names(prom_seq) != '']
 
 opts <- list()
 opts[["collection"]] <- "CORE"
